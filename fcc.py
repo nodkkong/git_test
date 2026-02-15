d791194e95a718af09415d99b59771c4c967f9dd
nodkkong <kimnoah15@gmail.com>
2026-02-15 13:49:38 -0800

    organizing files

---
+++ fcc.py
@@ -0,0 +1,8 @@
+text = 'Hello World'
+shift = 3
+alphabet = 'abcdefghijklmnopqrstuvwxyz'
+
+for char in text.lower():
+    index = alphabet.find(char)
+    new_index = index + shift
+    print(new_index)
---
+++ note.py
@@ -0,0 +1,9 @@
+from test import Chef
+from ChineseChef import ChineseChef
+
+myChef = Chef()
+myChef.make_chicken()
+
+myChineseChef = ChineseChef()
+myChineseChef.make_special_dish()
+myChineseChef.make_fried_rice()
