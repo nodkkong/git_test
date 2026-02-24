b2a09abce7e911e2c0fe2364db9f6e2f9ec14449
nodkkong <kimnoah15@gmail.com>
2026-02-24 00:59:08 -0800

    02232026 study

--- note.py
+++ note.py
@@ -1,9 +1,10 @@
-from test import Chef
-from ChineseChef import ChineseChef
+set_a = {1, 2, 3, 4}
+set_b = {2, 3, 4, 5, 6}
 
-myChef = Chef()
-myChef.make_chicken()
-
-myChineseChef = ChineseChef()
-myChineseChef.make_special_dish()
-myChineseChef.make_fried_rice()
+#set_a.union(set_b)
+#set_a.intersection(set_b)
+#set_a.symmetric_difference(set_b)
+#set_a.difference(set_b)
+#set_a.issubset(set_b)
+#set_a.issuperset(set_b)
+print(set_a.issuperset(set_b))
